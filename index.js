964736c8a41f52c70181cde29eab420a3093072f
Wojciech Rosiński <106152622+RavenPl@users.noreply.github.com>
2023-03-02 00:26:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let firstNumber = prompt('Podaj pierwszą liczbę!');
-const operator = prompt('Podaj rodzaj działania: +, -, *, :, %');
+const operator = prompt('Podaj rodzaj działania: +, -, *, /, %');
 let secondNumber = prompt('Podaj drugą liczbę!');
 const allowedOperators = ['+', '-', '*', '/', '%'];
 let result;
 
 const calculator = (firstNumber, secondNumber, operator) => {
-    const parsedFirstNumber = Number([...firstNumber].map(letter => letter === "," ? "." : letter).join(''));
-    const parsedSecondNumber = Number([...secondNumber].map(letter => letter === "," ? "." : letter).join(''));
+    const parsedFirstNumber = Number([...firstNumber].map(el => el === "," ? "." : el).join(''));
+    const parsedSecondNumber = Number([...secondNumber].map(el => el === "," ? "." : el).join(''));
 
     if (
         isNaN(parsedFirstNumber) ||
@@ -22,7 +22,7 @@ const calculator = (firstNumber, secondNumber, operator) => {
     result = calc(parsedFirstNumber, parsedSecondNumber, operator);
 
     if (result) {
-        const anotherOperator = prompt('Jeśli chcesz kontynuować to wybierz operator');
+        const anotherOperator = prompt('Jeśli chcesz kontynuować to wybierz kolejny operator!');
         if (allowedOperators.includes(anotherOperator)) {
             secondNumber = prompt(`Podaj kolejną liczbę! Aktualny wynik to: ${result}`);
             calculator(result, secondNumber, anotherOperator);
